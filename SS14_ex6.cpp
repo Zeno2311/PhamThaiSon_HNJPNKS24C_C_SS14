66ce1bcddd41bb476a8c934428220c9937a90e77
Zeno2311 <shanks23k6@gmail.com>
2024-12-12 10:49:04 +0700

    Update SS14_ex6.cpp

--- SS14_ex6.cpp
+++ SS14_ex6.cpp
@@ -3,20 +3,16 @@
 
 int main(){
 	char sigma[50];
-	int number = 0;
 	int all;
 	
 	printf("Moi ban nhap chuoi ki tu : ");
 	fgets(sigma , 50 , stdin);
-	
 	for(int i = 0 ; i < strlen(sigma) ; i++){
-		for(int j=0 ; j <= 9 ; j++){
-			if(sigma[i] == j){
-				number++;
-			}
+		if(sigma[i] >= 'a' && 'z' <= sigma[i] || sigma[i] >= 'A' && 'Z' <= sigma[i]){
+			all++;	
 		}
 	}
-	all = strlen(sigma) - 2 - number ;
+	
 	printf("So luong la chu cai trong chuoi la %d", all);
 	return 0;
 }
